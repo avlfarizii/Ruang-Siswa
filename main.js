50b3a47b862ffe02808f5c0bbc7644cbe2a33d5c
avlfarizii <139940852+avlfarizii@users.noreply.github.com>
2024-02-04 17:32:56 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function toggleResponsiveNavbar() {
+    var navbar = document.getElementById("myNavbar");
+    if (navbar.className === "navbar") {
+        navbar.className += " responsive";
+    } else {
+        navbar.className = "navbar";
+    }
+}
